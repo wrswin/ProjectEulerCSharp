b212a9dd5f90790f175af8491a544c6014a6098b
Wayde Reitsma <102506005@student.swin.edu.au>
2019-03-28 17:37:36 +1100

    Add problem 6 solution

---
+++ Problem6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem6 {
+    class Program {
+        static void Main(string[] args) {
+            var sumOfSquares = 0;
+            var squareOfSum = 0;
+
+            for(var i = 1; i <= 100; i += 1) {
+                sumOfSquares += i * i;
+                squareOfSum += i;
+            }
+
+            squareOfSum *= squareOfSum;
+
+            Console.WriteLine(squareOfSum - sumOfSquares);
+        }
+    }
+}
