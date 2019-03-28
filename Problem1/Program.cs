f0cd5f53d3d32a80f7b63fd829fd59d0f5197ff3
Wayde Reitsma <102506005@student.swin.edu.au>
2019-03-28 15:12:45 +1100

    Add problem 1 solution

---
+++ Problem1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            var sum = 0;
+
+            for(var i = 0; i < 1000; i += 1) {
+                if(i % 3 == 0 || i % 5 == 0) {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
