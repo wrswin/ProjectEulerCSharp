2ea87fb1e5ec4302e90ba25837e1e9c5ee787f5b
Wayde Reitsma <102506005@student.swin.edu.au>
2019-03-28 15:17:42 +1100

    Add problem 2 solution

---
+++ Problem2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem2 {
+    class Program {
+        static void Main(string[] args) {
+            var previousPreviousNumber = 1;
+            var previousNumber = 2;
+
+            var sum = 2;
+
+            while(previousNumber < 4000000) {
+                var number = previousPreviousNumber + previousNumber;
+
+                if(number % 2 == 0) {
+                    sum += number;
+                }
+
+                previousPreviousNumber = previousNumber;
+                previousNumber = number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
