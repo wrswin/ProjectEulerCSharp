868c9c443f3ed0f8170988eff08738845744c718
Wayde Reitsma <102506005@student.swin.edu.au>
2019-03-28 17:47:31 +1100

    Add problem 7 solution

---
+++ Problem7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Problem7 {
+    class Program {
+        static void Main(string[] args) {
+            var primeCount = 0;
+            var i = 2;
+
+            while(true) {
+                var prime = true;
+
+                for(var j = 2; j < i / 2; j += 1) {
+                    if(i % j == 0) {
+                        prime = false;
+
+                        break;
+                    }
+                }
+
+                if(prime) {
+                    if(primeCount == 10001) {
+                        Console.WriteLine(i);
+
+                        break;
+                    }
+
+                    primeCount += 1;
+                }
+
+                i += 1;
+            }
+
+            
+        }
+    }
+}
