c38ccf53c4ba35e3beb65b53838fe9f3989f2b8a
Wayde Reitsma <102506005@student.swin.edu.au>
2019-03-28 17:33:58 +1100

    Add problem 5 solution

---
+++ Problem5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Problem5 {
+    class Program {
+        static void Main(string[] args) {
+            var i = 1;
+            while(true) {
+                var found = true;
+
+                for(var j = 1; j < 20; j += 1) {
+                    if(i % j != 0) {
+                        found = false;
+
+                        break;
+                    }
+                }
+
+                if(found) {
+                    Console.WriteLine(i);
+
+                    break;
+                } else {
+                    i += 1;
+                }
+            }
+        }
+    }
+}
