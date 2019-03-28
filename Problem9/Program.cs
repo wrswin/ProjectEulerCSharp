1d8b8d091dc712a63b5ecce779f27c4d3818d631
Wayde Reitsma <102506005@student.swin.edu.au>
2019-03-28 18:13:30 +1100

    Add problem 9 solution

---
+++ Problem9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem9 {
+    class Program {
+        static void Main(string[] args) {
+            for(var a = 1; a < 1000; a += 1) {
+                for(var b = a + 1; b < 1000; b += 1) {
+                    for(var c = b + 1; c < 1000; c += 1) {
+                        if(a * a + b * b == c * c && a + b + c == 1000) {
+                            Console.WriteLine(a * b * c);
+
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
