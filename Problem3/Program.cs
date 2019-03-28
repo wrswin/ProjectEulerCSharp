922f0f320ec0f4d95671147eb827f1b4515ce13e
Wayde Reitsma <102506005@student.swin.edu.au>
2019-03-28 17:16:10 +1100

    Add problem 3 solution

---
+++ Problem3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem3 {
+    class Program {
+        static void Main(string[] args) {
+            var number = 600851475143;
+
+            for(long i = number / 2; i > 1; i -= 1) {
+                if(number % i == 0) {
+                    var prime = true;
+
+                    for(long j = 2; j < i / 2; j += 1) {
+                        if(i % j == 0) {
+                            prime = false;
+
+                            break;
+                        }
+                    }
+
+                    if(prime) {
+                        Console.WriteLine(i);
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
